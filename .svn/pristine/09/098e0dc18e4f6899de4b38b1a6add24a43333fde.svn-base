c08aa81f12da54ac3e840676ae2ae2375ddec067
jingChangXiao <xiaojingchang@chelizitech.com>
2018-04-25 10:15:32 +0800

    creact

---
+++ .svn/pristine/09/098e0dc18e4f6899de4b38b1a6add24a43333fde.svn-base
@@ -0,0 +1,14 @@
+const calendars = (resolve) => { require(['@/pages/class/calendars'], resolve) }
+const addSchedu = (resolve) => { require(['@/pages/class/addSchedu'], resolve) }
+export default [
+  {
+    name: 'calendars',
+    component: calendars,
+    path: '/calendars'
+  },
+  {
+    name: 'addSchedu',
+    component: addSchedu,
+    path: '/addSchedu'
+  }
+]
---
+++ src/utils/app.js
@@ -0,0 +1,47 @@
+document.addEventListener('deviceready', onDeviceready)
+window.JPush.init()
+window.JPush.setDebugMode(true)
+function onDeviceready () {
+  alert('aaaa')
+  document.addEventListener('jpush.receiveRegistrationId', function (event) {
+    alert('receiveRegistrationId' + JSON.stringify(event))
+    try {
+      window.JPush.init()
+      window.JPush.setDebugMode(true)
+
+      if (window.device.platform !== 'Android') {
+        window.JPush.setApplicationIconBadgeNumber(0)
+      }
+    } catch (exception) {}
+  }, false)
+}
+document.addEventListener('jpush.openNotification', onOpenNotification, false)
+document.addEventListener('jpush.receiveNotification', onReceiveNotification, false)
+function onOpenNotification (event) {
+  alert(event)
+  try {
+    var alertContent
+    if (window.device.platform === 'Android') {
+      alertContent = event.alert
+    } else {
+      alertContent = event.aps.alert
+    }
+    alert('open Notification:' + alertContent)
+  } catch (exception) {
+    console.log('JPushPlugin:onOpenNotification' + exception)
+  }
+}
+function onReceiveNotification (event) {
+  alert(event)
+  try {
+    var alertContent
+    if (window.device.platform === 'Android') {
+      alertContent = event.alert
+    } else {
+      alertContent = event.aps.alert
+    }
+    alert('open Notification:' + alertContent)
+  } catch (exception) {
+    console.log('JPushPlugin:onOpenNotification' + exception)
+  }
+}
